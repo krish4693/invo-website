b5bd8bb0e155a178bd93a1b67cfd3b175614c68a
krish4693 <krishnanandunnikrishnan@gmail.com>
2024-04-20 12:17:09 +0530

    new theme-added

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -23,41 +23,45 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 
 export function ThemeProvider({
   children,
-  defaultTheme = 'system',
-  storageKey = 'theme',
+  defaultTheme = "dark",
+  storageKey = "theme",
   ...props
-}:ThemeProviderProps)   {
-  const [theme, setTheme] = useState(defaultTheme);
+}: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>(
+    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
+  );
 
   useEffect(() => {
     const root = window.document.documentElement;
 
-    root.classList.remove('light', 'dark');
+    root.classList.remove("light", "dark");
 
-    if (theme === 'system') {
-      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
+    if (theme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
         .matches
-        ? 'dark'
-        : 'light';
+        ? "dark"
+        : "light";
 
       root.classList.add(systemTheme);
-    } else {
-      root.classList.add(theme);
+      return;
     }
-  }, [theme]);
 
-  const updateTheme = (newTheme:any) => {
-    localStorage.setItem(storageKey, newTheme);
-    setTheme(newTheme);
-  };
+    root.classList.add(theme);
+  }, [theme]);
 
   const value = {
     theme,
-    setTheme: updateTheme,
+    setTheme: (theme: Theme) => {
+      localStorage.setItem(storageKey, theme);
+      setTheme(theme);
+    },
   };
 
   return (
-    <ThemeProviderContext.Provider value={value} {...props}>
+    <ThemeProviderContext.Provider
+      {...props}
+      value={value}
+    >
       {children}
     </ThemeProviderContext.Provider>
   );
