adc0f297fa3d51ecd96390565fd9e9c89a989139
krish4693 <krishnanandunnikrishnan@gmail.com>
2024-04-20 15:24:23 +0530

    modified hero-color gradient of hero text h1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,20 +19,20 @@ export const Hero = () => {
   return (
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       
-      <div className="text-center lg:text-start space-y-6">
-        <main className="text-5xl md:text-6xl font-bold">
+      <div className="text-center lg:text-start space-y-6  ">
+        <main className="text-5xl md:text-6xl font-bold ">
           <h1 className="inline">
             <span>
               Simplified
             </span>{" "}
-            <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
+            <span className="dark:bg-gradient-to-r dark:from-white dark:to-gray-400 dark:text-transparent dark:bg-clip-text">
             Inventory
 
             </span>
           </h1>{" "}
           and{" "}
           <h2 className="inline">
-            <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
+            <span className=" dark:bg-gradient-to-r dark:from-white dark:to-gray-400 dark:text-transparent dark:bg-clip-text">
               Invoice
             </span>{" "}
             Management
