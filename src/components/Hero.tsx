2c2785a9e2e18541ede9de1e1421a0176423117d
krish4693 <krishnanandunnikrishnan@gmail.com>
2024-04-20 16:24:31 +0530

    added newfont to hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,8 +20,8 @@ export const Hero = () => {
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       
       <div className="text-center lg:text-start space-y-6  ">
-        <main className="text-5xl md:text-6xl font-bold ">
-          <h1 className="inline">
+        <main className="text-5xl md:text-6xl font-bold pacifico-regular">
+          <h1 className="inline ">
             <span>
               Simplified
             </span>{" "}
